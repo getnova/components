a07775e1fe4babdb6e328895ec8c97d84f1b75e9
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-01-29 17:47:15 +0100

    Enabled unit tests in actions

--- projects/getnova-components/src/controls/text-field/text-field-ref.directive.spec.ts
+++ projects/getnova-components/src/controls/text-field/text-field-ref.directive.spec.ts
@@ -1,8 +1,10 @@
 import {TextFieldRefDirective} from './text-field-ref.directive';
+import {ElementRef} from '@angular/core';
 
 describe('TextFieldRefDirective', () => {
   it('should create an instance', () => {
-    const directive = new TextFieldRefDirective();
+    const eleRef: ElementRef = {nativeElement: {value: ''}};
+    const directive = new TextFieldRefDirective(eleRef);
     expect(directive).toBeTruthy();
   });
 });
